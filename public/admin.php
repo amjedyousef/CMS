400432f4d22f8a00e60fa22a748acd53c2da6686
Amjed <com_amjed@yahoo.com>
2015-09-15 10:44:59 +0200

    initial commit

---
+++ public/admin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+	<title>Web title</title>
+</head>
+<body>
+
+<h1>Admin Menu</h1>
+
+<div id="sideMenu">
+	<ul>
+		<li> 
+			<a href=""> 
+				Manageing Web content
+			</a>
+		</li>
+		<li> 
+			<a href=""> 
+				Manageing Admin Users
+			</a>
+		</li>
+		<li> 
+			<a href=""> 
+				Logout
+			</a>
+		</li>
+	</ul>
+</div>
+
+<div id="mainContent">
+	<h2>Main content </h2>
+	
+</div>
+
+<footer>
+	<h2>Footer</h2>
+</footer>
+</body>
+</html>
